9b96a95891adc0a2fad1d3526e79d692173241a2
firasnouri28 <84795395+firasnouri28@users.noreply.github.com>
2021-05-25 20:05:15 +0200

    Update index.js

--- src/components/Camera/index.js
+++ src/components/Camera/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import ReactPlayer from 'react-player'
 const Camera = () => {
     return (
         <div className='camera'>
-            <h3 style={{textAlign:'center'}}>Access Camera</h3>
+            <h3 style={{textAlign:'center'}}>Access à la Camera</h3>
+             <video controls muted>
+                <source src='http://169.254.111.226:5000/video_feed' ></source>
+          
+            </video> 
+            
         </div>
     )
 }
